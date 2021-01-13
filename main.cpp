de563ad957290222de30fa7e8162eaafb7bdb3ff
TypeA2 <nuanlim29@gmail.com>
2021-01-13 14:18:42 +0100

    Actual testing

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <array>
 #include <string>
+#include <chrono>
 
 #ifdef _MSC_VER
 // MSVC typeid(T).name() is unmangled
@@ -26,54 +27,67 @@ std::string type_name() {
 #endif
 
 namespace test {
+	template <bool print>
 	struct helper {
 		std::size_t value{};
 
 		helper(std::size_t v) : value{ v } {
-			std::cout << "    [helper object] Constructor called with v = " << v << '\n';
+			if constexpr (print) {
+				std::cout << "    [helper object] Constructor called with v = " << v << '\n';
+			}
 		}
 
 		~helper() {
-			std::cout << "    [helper object] Destructor called, v = " << value << '\n';
+			if constexpr (print) {
+				std::cout << "    [helper object] Destructor called, v = " << value << '\n';
+			}
 		}
 	};
 
-	template <typename T1, typename T2, typename T3>
+	template <typename T1, typename T2, typename T3, bool print>
 	struct helper2 {
 		T1 v1;
 		T2 v2;
 		T3 v3;
 
 		helper2(T1 t1, T2 t2, T3 t3) : v1{ std::move(t1) }, v2{ std::move(t2) }, v3 { std::move(t3) } {
-			std::cout << "    [helper2 object] Constructor called with:\n"
-				"        " << *v1 << "\n"
-				"        " << *v2 << "\n"
-				"        " << *v3 << '\n';
+			if constexpr (print) {
+				std::cout << "    [helper2 object] Constructor called with:\n"
+					"        " << *v1 << "\n"
+					"        " << *v2 << "\n"
+					"        " << *v3 << '\n';
+			}
 		}
 
 		~helper2() {
-			std::cout << "    [helper2 object] Destructor called with: \n"
-				"        " << *v1 << "\n"
-				"        " << *v2 << "\n"
-				"        " << *v3 << '\n';
+			if constexpr (print) {
+				std::cout << "    [helper2 object] Destructor called with: \n"
+					"        " << *v1 << "\n"
+					"        " << *v2 << "\n"
+					"        " << *v3 << '\n';
+			}
 		}
 	};
 
-	template <typename T1, typename T2>
+	template <typename T1, typename T2, bool print>
 	struct helper3 {
 		T1 v1;
 		T2 v2;
 
 		helper3(T1 t1, T2 t2) : v1{ std::move(t1) }, v2{ std::move(t2) } {
-			std::cout << "    [helper3 object] Constructor called with:\n"
-				"        " << *v1 << "\n"
-				"        " << *v2.lock() << '\n';
+			if constexpr (print) {
+				std::cout << "    [helper3 object] Constructor called with:\n"
+					"        " << *v1 << "\n"
+					"        " << *v2.lock() << '\n';
+			}
 		}
 
 		~helper3() {
-			std::cout << "    [helper3 object] Destructor called with:\n"
-				"        " << *v1 << "\n"
-				"        " << *v2.lock() << '\n';
+			if constexpr (print) {
+				std::cout << "    [helper3 object] Destructor called with:\n"
+					"        " << *v1 << "\n"
+					"        " << *v2.lock() << '\n';
+			}
 		}
 	};
 }
@@ -86,11 +100,11 @@ std::ostream& operator<<(std::ostream& os, const std::array<T, N>& arr) {
 	return os;
 }
 
-template <typename T>
+template <typename T, bool print>
 bool test_allocator() {
 	const auto allocator_name = type_name<T>();
 
-	auto ident = [](std::size_t i = 0) -> std::ostream& {
+	auto ident = [&](std::size_t i = 0) -> std::ostream& {
 		return std::cout << std::string(i * 4, ' ');
 	};
 	
@@ -100,68 +114,99 @@ bool test_allocator() {
 
 	try {
 		T alloc;
-		
-		log() << "Starting tests...\n";
+
+		if constexpr (print) {
+			ident() << "Starting tests...\n";
+		}
 		
 		{
-			ident(1) << "Basic object allocation and deallocation\n";
-			auto x = alloc.template allocate<test::helper>(42);
-			ident(1) << "Allocated at " << x.get() << ", value: " << x->value << '\n';
+			if constexpr (print) {
+				ident(1) << "Basic object allocation and deallocation\n";
+			}
+			
+			auto x = alloc.template allocate<test::helper<print>>(42);
+			
+			if constexpr (print) {
+				ident(1) << "Allocated at " << x.get() << ", value: " << x->value << '\n';
+			}
 		}
 
-		ident() << '\n';
+		if constexpr (print) {
+			ident() << '\n';
+		}
 		
 		{
-			ident(1) << "Basic object, with refcount increase\n";
+			if constexpr (print) {
+				ident(1) << "Basic object, with refcount increase\n";
+			}
+			
 			// 1 instance and 4 copies
-			auto instance = alloc.template allocate<test::helper>(43);
+			auto instance = alloc.template allocate<test::helper<print>>(43);
 
 			{
 				std::remove_cvref_t<
-					decltype(alloc.template allocate<test::helper>(42))> elements[4];
+					decltype(alloc.template allocate<test::helper<print>>(42))> elements[4];
 				
 				for (auto& c : elements) {
 					c = instance;
 				}
 
-				ident(1) << "Values:\n";
-				for (const auto& c : elements) {
-					ident(2) << "value = " << c->value << '\n';
-				}
+				if constexpr (print) {
+					ident(1) << "Values:\n";
+					for (const auto& c : elements) {
+						ident(2) << "value = " << c->value << '\n';
+					}
 
-				ident(1) << "Deleting copies\n";
+					ident(1) << "Deleting copies\n";
+				}
 			}
-			
-			ident(1) << "Deleting final reference\n";
+
+			if constexpr (print) {
+				ident(1) << "Deleting final reference\n";
+			}
 		}
 
-		ident() << '\n';
+		if constexpr (print) {
+			ident() << '\n';
+		}
 		
 		{
-			ident(1) << "Moving from 1 instance to another\n";
-			auto x = alloc.template allocate<test::helper>(44);
-
-			ident(1) << "Address and value held by old object: "
-				<< x->value << " at " << x.get() << '\n';
+			if constexpr (print) {
+				ident(1) << "Moving from 1 instance to another\n";
+			}
 			
-			ident(1) << "Moving...\n";
+			auto x = alloc.template allocate<test::helper<print>>(44);
+
+			if constexpr (print) {
+				ident(1) << "Address and value held by old object: "
+					<< x->value << " at " << x.get() << '\n';
+
+				ident(1) << "Moving...\n";
+			}
+			
 			auto y = std::move(x);
 
-			ident(1) << "Address and value held by new object: "
-				<< y->value << " at " << y.get() << '\n';
+			if constexpr (print) {
+				ident(1) << "Address and value held by new object: "
+					<< y->value << " at " << y.get() << '\n';
 
-			ident(1) << "Deleting\n";
+				ident(1) << "Deleting\n";
+			}
 		}
 
-		ident() << '\n';
+		if constexpr (print) {
+			ident() << '\n';
+		}
 
 		{
-			ident(1) << "Creating a struct with multiple, nested members\n";
+			if constexpr (print) {
+				ident(1) << "Creating a struct with multiple, nested members\n";
+			}
 
 			using int_field = typename T::template object_type<int>;
 			using string_field = typename T::template object_type<std::string>;
 			using float_field = typename T::template object_type<std::array<float, 8>>;
-			using struct_type = test::helper2<int_field, string_field, float_field>;
+			using struct_type = test::helper2<int_field, string_field, float_field, print>;
 
 			auto s = alloc.template allocate<struct_type>(
 				alloc.template allocate<int>(45),
@@ -172,41 +217,63 @@ bool test_allocator() {
 					std::array<float, 8> { 1.f, 1.f, 2.f, 3.f, 5.f, 8.f, 13.f, 21.f })
 				);
 
-			ident(1) << "Deallocating said struct.\n";
+			if constexpr (print) {
+				ident(1) << "Deallocating said struct.\n";
+			}
 		}
 
-		ident() << '\n';
+		if constexpr (print) {
+			ident() << '\n';
+		}
 
 		{
-			ident(1) << "Testing weak references.\n";
+			if constexpr (print) {
+				ident(1) << "Testing weak references.\n";
+			}
 
-			typename T::template weak_type<test::helper> weak1;
+			typename T::template weak_type<test::helper<print>> weak1;
 			{
-				ident(1) << "Creating strong reference and assigning weak reference.\n";
+				if constexpr (print) {
+					ident(1) << "Creating strong reference and assigning weak reference.\n";
+				}
 
-				auto strong = alloc.template allocate<test::helper>(46);
+				auto strong = alloc.template allocate<test::helper<print>>(46);
 				weak1 = strong;
 
 				{
-					ident(1) << "Creating second weak reference.\n";
+					if constexpr (print) {
+						ident(1) << "Creating second weak reference.\n";
+					}
+					
 					decltype(weak1) weak2 = strong;
-					ident(1) << "Second weak reference going out of scope.\n";
+					
+					if constexpr (print) {
+						ident(1) << "Second weak reference going out of scope.\n";
+					}
 				}
 
-				ident(1) << "Strong reference going out of scope.\n";
+				if constexpr (print) {
+					ident(1) << "Strong reference going out of scope.\n";
+				}
 			}
 
-			ident(1) << "Last weak reference going out of scope.\n";
+			if constexpr (print) {
+				ident(1) << "Last weak reference going out of scope.\n";
+			}
 		}
-		
-		ident() << '\n';
+
+		if constexpr (print) {
+			ident() << '\n';
+		}
 
 		{
-			ident(1) << "Creating a struct with 1 strong and 1 weak pointer.\n";
+			if constexpr (print) {
+				ident(1) << "Creating a struct with 1 strong and 1 weak pointer.\n";
+			}
 
 			using strong_field = typename T::template object_type<std::string>;
 			using weak_field = typename T::template weak_type<std::string>;
-			using struct_type = test::helper3<strong_field, weak_field>;
+			using struct_type = test::helper3<strong_field, weak_field, print>;
 
 			typename T::template object_type<struct_type> obj;
 
@@ -215,10 +282,15 @@ bool test_allocator() {
 
 				// Make strong and weak pointer
 				obj = alloc.template allocate<struct_type>(ptr, ptr);
-				ident(1) << "Deallocating temporary object\n";
+
+				if constexpr (print) {
+					ident(1) << "Deallocating temporary object\n";
+				}
 			}
 
-			ident(1) << "Deallocating struct\n";
+			if constexpr (print) {
+				ident(1) << "Deallocating struct\n";
+			}
 		}
 
 	} catch (std::exception& e) {
@@ -229,12 +301,51 @@ bool test_allocator() {
 	return true;
 }
 
+std::ostream& operator<<(std::ostream& os, const std::chrono::nanoseconds& ns) {
+	auto count = ns.count();
+	if (count > 1'000'000'000) {
+		os << (count / 1'000'000'000.) << " s (" << count << " ns)";
+		
+	} else if (count > 1'000'000) {
+		os << (count / 1'000'000.) << " ms (" << count << " ns)";
+		
+	} else if (count > 1'000) {
+		os << (count / 1'000.) << " us (" << count << " ns)";
+		
+	} else {
+		os << count << " ns";
+	}
+
+	return os;
+}
+
 int main() {
 	bool success = true;
 
 	// Try all allocators
-	if (!test_allocator<gc::refcount::allocator>()) {
+	if (!test_allocator<gc::refcount::allocator, true>()) {
 		success = false;
+	}
+
+	
+
+	using clock = std::chrono::steady_clock;
+	constexpr size_t runs = 65535;
+	
+	std::cout <<
+		"\n\n======== Benchmarking... ========\n"
+		"    Iterations: " << runs << "\n\n";
+	
+	{
+		auto start = clock::now();
+
+		for (size_t i = 0; i < runs; ++i) {
+			(void) test_allocator<gc::refcount::allocator, false>();
+		}
+
+		auto end = clock::now();
+
+		std::cout << "[gc::refcount] " << (end - start) << '\n';;
 	}
 
 	return success ? EXIT_SUCCESS : EXIT_FAILURE;
